003efef2dec1cc4b5a2331ce7366199ddf451db1
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-09-29 20:14:09 +0200

    Redirect user to profile page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,9 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup () @user = User.new(name: "Example User", email: "user@example.com", password: "aaaaaa", password_confirmation: "aaaaaa"); end
+
+
   test "should get new" do
     get signup_path
     assert_response :success
